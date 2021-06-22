44d7c97ddafd532f460218c0656b234b302a4470
HaowenWeiJohn <hwei@wpi.edu>
2021-06-22 19:01:23 -0400

    training_script.sh uploading

---
+++ HPC_Training/2020_all_data_31class_corrupt_removal_with_minimax_simple_model/training_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name="2020_all_data_31class_corrupt_removal_with_minimax_simple_model"
+#SBATCH -D .
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=32G
+#SBATCH --cpus-per-task=2
+#SBATCH --gres=gpu:1
+#SBATCH --time=06:00:00
+
+
+module purge
+module load gcc/6.3.0 slurm/17.02.11 cudnn/8.1.1.33-11.2/3k5bbs63 cuda/11.0.2/ur4svoci 
+
+nvidia-smi
+echo $CUDA_VISIBLE_DEVICES
+python training_script.py
