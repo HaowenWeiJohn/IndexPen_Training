c5e7b1f5f1fe5b63389d80d18f3925c7adc3200d
HaowenWeiJohn <hwei@wpi.edu>
2021-06-22 22:49:08 -0400

    add test node running

---
+++ HPC_Training/test_running/training_script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name="test_running"
+#SBATCH -D .
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=50G
+#SBATCH --cpus-per-task=2
+#SBATCH --gres=gpu:1
+#SBATCH --partition=long
+#SBATCH --time=90:00:00
+
+
+module purge
+module load gcc/6.3.0 slurm/17.02.11 cudnn/8.1.1.33-11.2/3k5bbs63 cuda/11.0.2/ur4svoci 
+
+nvidia-smi
+echo $CUDA_VISIBLE_DEVICES
+python training_script.py
