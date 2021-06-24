dad9820c5340d5ead189a74ab44965b9d42c12cc
HaowenWeiJohn <hwei@wpi.edu>
2021-06-24 19:18:59 -0400

    retrain test

--- HPC_Testing/1-New_User_20_Sample_Increasing_Traing_Testing_Ratio/testing_script.py
+++ HPC_Testing/1-New_User_20_Sample_Increasing_Traing_Testing_Ratio/testing_script.py
@@ -1,8 +1,17 @@
-
-
-
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                stratify=y,
-                                                test_size=0.4)
+import datetime
+import glob
+import pickle
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.callbacks import CSVLogger
+from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
+import time
+
+import sys
+# insert at 1, 0 is the script path (or '' in REPL)
+sys.path.insert(1, '/work/hwei/HaowenWeiDeepLearning/IndexPenTrainingDir/IndexPen_Training')
 
