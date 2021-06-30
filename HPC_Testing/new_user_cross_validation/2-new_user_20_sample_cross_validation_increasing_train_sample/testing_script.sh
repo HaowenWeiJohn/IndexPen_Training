0a2ed053aead44d12c46e75ab03c5b39cbd8fb35
HaowenWeiJohn <hwei@wpi.edu>
2021-06-29 21:45:55 -0400

    fixing batch size err

---
+++ HPC_Testing/new_user_cross_validation/2-new_user_20_sample_cross_validation_increasing_train_sample/testing_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name="2-new_user_20_sample_cross_validation_increasing_train_sample"
+#SBATCH -D .
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=50G
+#SBATCH --cpus-per-task=2
+#SBATCH --gres=gpu:1
+#SBATCH --partition=long
+#SBATCH --time=30:00:00
+
+
+module purge
+module load gcc/6.3.0 slurm/17.02.11 cudnn/8.1.1.33-11.2/3k5bbs63 cuda/11.0.2/ur4svoci 
+
+
+python testing_script.py
