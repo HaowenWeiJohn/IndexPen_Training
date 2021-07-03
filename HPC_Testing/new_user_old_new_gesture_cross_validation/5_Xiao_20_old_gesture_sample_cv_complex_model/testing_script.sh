79a865edd78f5810d03d1b0cb0b98efbb051abd6
HaowenWeiJohn <hwei@wpi.edu>
2021-07-03 02:04:19 -0400

    test complex model transfer learning on xiao new samples

--- HPC_Testing/new_user_old_new_gesture_cross_validation/5_Xiao_20_old_gesture_sample_cv_complex_model/testing_script.sh
+++ HPC_Testing/new_user_old_new_gesture_cross_validation/5_Xiao_20_old_gesture_sample_cv_complex_model/testing_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name="4_Xiao_20_old_gesture_sample_cv"
+#SBATCH --job-name="5_Xiao_20_old_gesture_sample_cv_complex_model"
 #SBATCH -D .
 #SBATCH --output=%x_%j.out
 #SBATCH --error=%x_%j.err
