a3ab6941c049988a957cc18cdec830d125dcb8d3
HaowenWeiJohn <hwei@wpi.edu>
2021-07-04 02:46:56 -0400

    capacity increase simple model test

--- data_utils/make_model.py
+++ data_utils/make_model.py
@@ -84,32 +84,32 @@ def make_simple_model(class_num=31, learning_rate=1e-3, decay=1e-6):
 def make_simple_model_capacity_increase(class_num=31, learning_rate=1e-3, decay=1e-6):
     encoder1 = Sequential()
     encoder1.add(tf.keras.layers.InputLayer(input_shape=(120, 8, 16, 1)))
-    encoder1.add(TimeDistributed(Conv2D(filters=16, kernel_size=(2, 3),
-                                        kernel_regularizer=tf.keras.regularizers.l2(l=1e-5),
-                                        bias_regularizer=tf.keras.regularizers.l2(l=1e-5),
-                                        activity_regularizer=tf.keras.regularizers.l2(l=1e-5),
+    encoder1.add(TimeDistributed(Conv2D(filters=12, kernel_size=(2, 3),
+                                        # kernel_regularizer=tf.keras.regularizers.l2(l=1e-5),
+                                        # bias_regularizer=tf.keras.regularizers.l2(l=1e-5),
+                                        # activity_regularizer=tf.keras.regularizers.l2(l=1e-5),
                                         data_format='channels_last')))
-    encoder1.add(TimeDistributed(tf.keras.layers.LeakyReLU(alpha=0.15)))
+    encoder1.add(TimeDistributed(tf.keras.layers.LeakyReLU(alpha=0.1)))
 
     encoder1.add(TimeDistributed(MaxPooling2D(pool_size=2)))
     encoder1.add(TimeDistributed(BatchNormalization()))
     encoder1.add(TimeDistributed(Flatten()))  # or Flatten()
-    encoder1.add(TimeDistributed(Dense(64, activation='relu')))
+    encoder1.add(TimeDistributed(Dense(32, activation='relu')))
     # encoder1.add(TimeDistributed(Dropout(rate=0.2)))
 
     # ENcoder2
     encoder2 = Sequential()
     encoder2.add(tf.keras.layers.InputLayer(input_shape=(120, 8, 64, 1)))
-    encoder2.add(TimeDistributed(Conv2D(filters=32, kernel_size=(3, 3),
-                                        kernel_regularizer=tf.keras.regularizers.l2(l=1e-5),
-                                        bias_regularizer=tf.keras.regularizers.l2(l=1e-5),
-                                        activity_regularizer=tf.keras.regularizers.l2(l=1e-5),
+    encoder2.add(TimeDistributed(Conv2D(filters=24, kernel_size=(3, 3),
+                                        # kernel_regularizer=tf.keras.regularizers.l2(l=1e-5),
+                                        # bias_regularizer=tf.keras.regularizers.l2(l=1e-5),
+                                        # activity_regularizer=tf.keras.regularizers.l2(l=1e-5),
                                         data_format='channels_last')))
-    encoder2.add(TimeDistributed(tf.keras.layers.LeakyReLU(alpha=0.15)))
+    encoder2.add(TimeDistributed(tf.keras.layers.LeakyReLU(alpha=0.1)))
     encoder2.add(TimeDistributed(MaxPooling2D(pool_size=2)))
     encoder2.add(TimeDistributed(BatchNormalization()))
     encoder2.add(TimeDistributed(Flatten()))  # or Flatten()
-    encoder2.add(TimeDistributed(Dense(128, activation='relu')))
+    encoder2.add(TimeDistributed(Dense(64, activation='relu')))
     # encoder2.add(TimeDistributed(Dropout(rate=0.2)))
 
     merged = concatenate([encoder1.output, encoder2.output])
@@ -123,11 +123,11 @@ def make_simple_model_capacity_increase(class_num=31, learning_rate=1e-3, decay=
                       kernel_initializer='random_uniform',
                       return_sequences=False,
                       )(merged)
-    merged_out = Dropout(rate=0.2)(merged_out)
+    merged_out = Dropout(rate=0.25)(merged_out)
     merged_out = Dense(512,
                        activation='relu'
                        )(merged_out)
-    merged_out = Dropout(rate=0.4)(merged_out)
+    merged_out = Dropout(rate=0.25)(merged_out)
     merged_out = Dense(class_num, activation='softmax', kernel_initializer='random_uniform')(merged_out)
 
     model = Model(inputs=[encoder1.input, encoder2.input], outputs=merged_out)
