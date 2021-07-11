be127b494226e773d2dc1cb0da42f37e746e577b
HaowenWeiJohn <hwei@wpi.edu>
2021-07-11 01:55:31 -0400

    unit sample minimax normalization test

--- HPC_Analysis_Test/13-2020_all_data_clutter_removal_0.8_0.6_ratio_test_without_minimax_simple_model_unit_sample_minimax/training_script.py
+++ HPC_Analysis_Test/13-2020_all_data_clutter_removal_0.8_0.6_ratio_test_without_minimax_simple_model_unit_sample_minimax/training_script.py
@@ -18,7 +18,8 @@ sys.path.insert(1, '/work/hwei/HaowenWeiDeepLearning/IndexPenTrainingDir/IndexPe
 from data_utils.make_model import *
 from data_utils.ploting import plot_confusion_matrix
 
-load_data_dir = '/work/hwei/HaowenWeiDeepLearning/IndexPenTrainingDir/IndexPen_Training/data/IndexPenData/IndexPenData2020/2020_31classes_corrupt_frame_removal_(-1000,1500)_(0,2500)_clutter_removal_(0.8)_(0.6)'
+# load_data_dir = '/work/hwei/HaowenWeiDeepLearning/IndexPenTrainingDir/IndexPen_Training/data/IndexPenData/IndexPenData2020/2020_31classes_corrupt_frame_removal_(-1000,1500)_(0,2500)_clutter_removal_(0.8)_(0.6)'
+load_data_dir = 'C:/Users/Haowe/OneDrive/Desktop/IndexPenData2020/2020_31classes_corrupt_frame_removal_(-1000,1500)_(0,2500)'
 with open(load_data_dir, 'rb') as f:
     X_mmw_rD, X_mmw_rA, Y, encoder = pickle.load(f)
 
@@ -41,6 +42,8 @@ for index in range(0, len(X_mmw_rA)):
 # X_mmw_rD = (X_mmw_rD - rD_min) / (rD_max - rD_min)
 # X_mmw_rA = (X_mmw_rA - rA_min) / (rA_max - rA_min)
 
+print('unit sample normalization')
+
 X_mmw_rD_train, X_mmw_rD_test, Y_train, Y_test = train_test_split(X_mmw_rD, Y, test_size=0.20, random_state=3,
                                                                   shuffle=True)
 del X_mmw_rD
@@ -50,7 +53,7 @@ X_mmw_rA_train, X_mmw_rA_test, Y_train, Y_test = train_test_split(X_mmw_rA, Y, t
 del X_mmw_rA
 del Y
 
-model = make_simple_model(class_num=31,  learning_rate=5e-4, decay=1e-6)
+model = make_simple_model(class_num=31,  learning_rate=5e-4, decay=1e-7)
 
 es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=60)
 csv_logger = CSVLogger("model_history_log.csv", append=True)
