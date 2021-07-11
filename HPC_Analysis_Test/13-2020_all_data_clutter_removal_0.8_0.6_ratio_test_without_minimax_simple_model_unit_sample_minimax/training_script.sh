52fc8226a575d4273263d635fa6808e99fcd843d
HaowenWeiJohn <hwei@wpi.edu>
2021-07-11 01:24:38 -0400

    unit sample minimax normalization test

--- HPC_Analysis_Test/13-2020_all_data_clutter_removal_0.8_0.6_ratio_test_without_minimax_simple_model_unit_sample_minimax/training_script.sh
+++ HPC_Analysis_Test/13-2020_all_data_clutter_removal_0.8_0.6_ratio_test_without_minimax_simple_model_unit_sample_minimax/training_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name="7-2020_all_data_clutter_removal_0.8_0.6_ratio_test_without_minimax_simple_model"
+#SBATCH --job-name="13-2020_all_data_clutter_removal_0.8_0.6_ratio_test_without_minimax_simple_model_unit_sample_minimax"
 #SBATCH -D .
 #SBATCH --output=%x_%j.out
 #SBATCH --error=%x_%j.err
