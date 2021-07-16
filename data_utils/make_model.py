a677e3c8945ea8191b5f3ece8e79edc39c2d5830
HaowenWeiJohn <hwei@wpi.edu>
2021-07-15 23:57:50 -0400

    user study  data  test

--- data_utils/make_model.py
+++ data_utils/make_model.py
@@ -87,6 +87,63 @@ def make_simple_model(class_num=31, learning_rate=1e-3, decay=1e-6, rd_kernel_si
     model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
     return model
 
+
+def make_simple_model_reg(class_num=31, learning_rate=1e-3, decay=1e-6, rd_kernel_size=(2, 3), ra_kernel_size=(3, 3)):
+    encoder1 = Sequential()
+    encoder1.add(tf.keras.layers.InputLayer(input_shape=(120, 8, 16, 1)))
+    encoder1.add(TimeDistributed(Conv2D(filters=8, kernel_size=rd_kernel_size,
+                                        kernel_regularizer=tf.keras.regularizers.l2(l=1e-6),
+                                        bias_regularizer=tf.keras.regularizers.l2(l=1e-6),
+                                        activity_regularizer=tf.keras.regularizers.l2(l=1e-6),
+                                        data_format='channels_last')))
+    encoder1.add(TimeDistributed(tf.keras.layers.LeakyReLU(alpha=0.1)))
+
+    encoder1.add(TimeDistributed(MaxPooling2D(pool_size=2)))
+    encoder1.add(TimeDistributed(BatchNormalization()))
+    encoder1.add(TimeDistributed(Flatten()))  # or Flatten()
+    encoder1.add(TimeDistributed(Dense(32, activation='relu')))
+    # encoder1.add(TimeDistributed(Dropout(rate=0.2)))
+
+    # ENcoder2
+    encoder2 = Sequential()
+    encoder2.add(tf.keras.layers.InputLayer(input_shape=(120, 8, 64, 1)))
+    encoder2.add(TimeDistributed(Conv2D(filters=16, kernel_size=ra_kernel_size,
+                                        kernel_regularizer=tf.keras.regularizers.l2(l=1e-6),
+                                        bias_regularizer=tf.keras.regularizers.l2(l=1e-6),
+                                        activity_regularizer=tf.keras.regularizers.l2(l=1e-6),
+                                        data_format='channels_last')))
+    encoder2.add(TimeDistributed(tf.keras.layers.LeakyReLU(alpha=0.1)))
+    encoder2.add(TimeDistributed(MaxPooling2D(pool_size=2)))
+    encoder2.add(TimeDistributed(BatchNormalization()))
+    encoder2.add(TimeDistributed(Flatten()))  # or Flatten()
+    encoder2.add(TimeDistributed(Dense(64, activation='relu')))
+    # encoder2.add(TimeDistributed(Dropout(rate=0.2)))
+
+    merged = concatenate([encoder1.output, encoder2.output])
+    # merged_out = LSTM(32, return_sequences=True, kernel_initializer='random_uniform',
+    #                   kernel_regularizer=tf.keras.regularizers.l2(l=1e-4),
+    #                   recurrent_regularizer=tf.keras.regularizers.l2(l=1e-5),
+    #                   activity_regularizer=tf.keras.regularizers.l2(l=1e-5)
+    #                   )(merged)
+    # merged_out = Dropout(rate=0.2)(merged_out)
+    merged_out = LSTM(32,
+                      kernel_initializer='random_uniform',
+                      return_sequences=False,
+                      )(merged)
+    merged_out = Dropout(rate=0.2)(merged_out)
+    merged_out = Dense(256,
+                       activation='relu'
+                       )(merged_out)
+    merged_out = Dropout(rate=0.2)(merged_out)
+    merged_out = Dense(class_num, activation='softmax', kernel_initializer='random_uniform')(merged_out)
+
+    model = Model(inputs=[encoder1.input, encoder2.input], outputs=merged_out)
+
+    adam = tf.keras.optimizers.Adam(learning_rate=learning_rate, decay=decay)
+    model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
+    return model
+
+
 def make_simple_model_capacity_increase(class_num=31, learning_rate=5e-4, decay=5e-7, rd_kernel_size=(2, 3), ra_kernel_size=(3, 3)):
     encoder1 = Sequential()
     encoder1.add(tf.keras.layers.InputLayer(input_shape=(120, 8, 16, 1)))
