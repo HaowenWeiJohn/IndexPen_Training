679f9ea0825c9791665e52b4aca64548ac838b79
HaowenWeiJohn <hwei@wpi.edu>
2021-07-16 01:04:25 -0400

    user study  data  test

---
+++ HPC_User_Study1_Test/0_2_all_data_test_simple_model_reg/training_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name="0_1_all_data_test_complex_model"
+#SBATCH -D .
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=60G
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --partition=long
+#SBATCH --time=60:00:00
+
+
+module purge
+module load gcc/6.3.0 slurm/17.02.11 cudnn/8.1.1.33-11.2/3k5bbs63 cuda/11.0.2/ur4svoci 
+
+
+python training_script.py
