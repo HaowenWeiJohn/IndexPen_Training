330881c18e05c0e34572ee9bbc8af89101e8dd09
HaowenWeiJohn <hwei@wpi.edu>
2021-07-16 20:12:03 -0400

    rename error dir

--- HPC_User_Study1_Test/0_2_all_data_test_complex_model_high_lr_confirm/training_script.sh
+++ HPC_User_Study1_Test/0_2_all_data_test_complex_model_high_lr_confirm/training_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name="0_2_all_data_test_complex_model_high_lr"
+#SBATCH --job-name="0_2_all_data_test_complex_model_high_lr_confirm"
 #SBATCH -D .
 #SBATCH --output=%x_%j.out
 #SBATCH --error=%x_%j.err
