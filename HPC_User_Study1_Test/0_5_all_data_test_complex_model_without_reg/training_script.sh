206d995feb7fa6606c1e0e37355e112fdfbffde2
HaowenWeiJohn <hwei@wpi.edu>
2021-07-16 21:45:03 -0400

    without reg complex model and test ready

--- HPC_User_Study1_Test/0_5_all_data_test_complex_model_without_reg/training_script.sh
+++ HPC_User_Study1_Test/0_5_all_data_test_complex_model_without_reg/training_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name="0_5_all_data_test_simple_model_noise_augmentation"
+#SBATCH --job-name="0_5_all_data_test_complex_model_without_reg"
 #SBATCH -D .
 #SBATCH --output=%x_%j.out
 #SBATCH --error=%x_%j.err
