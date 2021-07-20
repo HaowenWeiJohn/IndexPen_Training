0ff33d59885aa037bdb0bc57449d10d7fe57f939
HaowenWeiJohn <hwei@wpi.edu>
2021-07-20 17:14:07 -0400

    all 2d cnn reg to complex model

--- HPC_User_Study_(0.8,0.8)_Train/7-20_Complex_Model_all_data_reg_all_large_kernal_size/training_script.py
+++ HPC_User_Study_(0.8,0.8)_Train/7-20_Complex_Model_all_data_reg_all_large_kernal_size/training_script.py
@@ -11,6 +11,7 @@ from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
 import time
 
 import sys
+
 # insert at 1, 0 is the script path (or '' in REPL)
 
 
@@ -20,7 +21,7 @@ from data_utils.data_config import *
 from data_utils.make_model import *
 from data_utils.ploting import plot_confusion_matrix
 
-load_data_dir = '../../data/IndexPenData/IndexPenStudyData/UserStudy1Data/7-17_all_study_data_cr_(0.8,0.8)'
+load_data_dir = '../../data/IndexPenData/IndexPenStudyData/UserStudy1Data/7-20_all_study_data_cr_(0.8,0.8)'
 # load all data and Y
 subject_name = 'Sub2_zs'
 
@@ -47,14 +48,14 @@ Y = []
 # load data
 subject_names = ['Sub1_hw', 'Sub2_zs', 'Sub3_ag', 'Sub4_xz']
 for subject_name in subject_names:
-        if len(X_mmw_rD) == 0:
-            X_mmw_rD = subjects_data_dict[subject_name][0]
-            X_mmw_rA = subjects_data_dict[subject_name][1]
-            Y = subjects_label_dict[subject_name]
-        else:
-            X_mmw_rD = np.concatenate([X_mmw_rD, subjects_data_dict[subject_name][0]])
-            X_mmw_rA = np.concatenate([X_mmw_rA, subjects_data_dict[subject_name][1]])
-            Y = np.concatenate([Y, subjects_label_dict[subject_name]])
+    if len(X_mmw_rD) == 0:
+        X_mmw_rD = subjects_data_dict[subject_name][0]
+        X_mmw_rA = subjects_data_dict[subject_name][1]
+        Y = subjects_label_dict[subject_name]
+    else:
+        X_mmw_rD = np.concatenate([X_mmw_rD, subjects_data_dict[subject_name][0]])
+        X_mmw_rA = np.concatenate([X_mmw_rA, subjects_data_dict[subject_name][1]])
+        Y = np.concatenate([Y, subjects_label_dict[subject_name]])
 
 # print(np.min(X_mmw_rD))
 # print(np.max(X_mmw_rD))
@@ -76,18 +77,20 @@ for subject_name in subject_names:
 # X_mmw_rA = (X_mmw_rA - rA_min) / (rA_max - rA_min)
 
 
-
-X_mmw_rD_train, X_mmw_rD_test, Y_train, Y_test = train_test_split(X_mmw_rD, Y, stratify=Y, test_size=0.20, random_state=3,
+X_mmw_rD_train, X_mmw_rD_test, Y_train, Y_test = train_test_split(X_mmw_rD, Y, stratify=Y, test_size=0.20,
+                                                                  random_state=3,
                                                                   shuffle=True)
 
-
-X_mmw_rA_train, X_mmw_rA_test, Y_train, Y_test = train_test_split(X_mmw_rA, Y, stratify=Y, test_size=0.20, random_state=3,
+X_mmw_rA_train, X_mmw_rA_test, Y_train, Y_test = train_test_split(X_mmw_rA, Y, stratify=Y, test_size=0.20,
+                                                                  random_state=3,
                                                                   shuffle=True)
 
+model = make_complex_model(class_num=31, learning_rate=1e-3, decay=2e-6,
+                           rd_kernel_size1=(3, 3), rd_kernel_size2=(3, 3),
+                           ra_kernel_size1=(3, 6), ra_kernel_size2=(3, 4),
+                           )
 
-model = make_simple_model_reg(class_num=31,  learning_rate=1e-3, decay=5e-6)
-
-es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=50)
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=250)
 csv_logger = CSVLogger("model_history_log.csv", append=True)
 mc = ModelCheckpoint(
     # filepath='AutoSave/' + str(datetime.datetime.now()).replace(':', '-').replace(' ',
@@ -96,7 +99,7 @@ mc = ModelCheckpoint(
     monitor='val_accuracy', mode='max', verbose=1, save_best_only=True)
 
 training_start_time = time.time()
-a = [X_mmw_rD_train, X_mmw_rA_train]
+
 history = model.fit([X_mmw_rD_train, X_mmw_rA_train], Y_train,
                     validation_data=([X_mmw_rD_test, X_mmw_rA_test], Y_test),
                     epochs=20000,
@@ -132,4 +135,3 @@ cm = plot_confusion_matrix(y_true=Y_test, y_pred=Y_pred, classes=indexpen_classe
 plt.savefig('confusion_matrix.png')
 test_acc = accuracy_score(Y_test, Y_pred)
 print("best_accuracy_score:", test_acc)
-
---
+++ HPC_User_Study_(0.8,0.8)_Train/7-20_Complex_Model_all_data_reg_all_large_kernal_size/training_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name="7-20_Complex_Model_all_data_reg_all_large_kernal_size"
+#SBATCH -D .
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=60G
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --partition=long
+#SBATCH --time=150:00:00
+
+
+module purge
+module load gcc/6.3.0 slurm/17.02.11 cudnn/8.1.1.33-11.2/3k5bbs63 cuda/11.0.2/ur4svoci 
+
+
+python training_script.py
