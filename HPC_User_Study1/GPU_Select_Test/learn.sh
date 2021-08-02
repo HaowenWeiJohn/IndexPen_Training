41dcf6338aa899ceabd5b052ff93912a3cb95d77
HaowenWeiJohn <hwei@wpi.edu>
2021-08-01 21:02:03 -0400

    select GPU test

--- HPC_User_Study1/GPU_Select_Test/learn.sh
+++ HPC_User_Study1/GPU_Select_Test/learn.sh
@@ -7,8 +7,7 @@
 #SBATCH --ntasks=1
 #SBATCH --mem=60G
 #SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:1
-#SBATCH -C "gpu_K40"
+#SBATCH --gres=gpu:E5-2667：1
 #SBATCH --partition=long
 #SBATCH --time=150:00:00
 
