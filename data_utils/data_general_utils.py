dede03534040d4b87cbb722783361a95223d0a94
HaowenWeiJohn <hwei@wpi.edu>
2021-09-21 00:38:24 -0400

    archive dtaidistance

--- data_utils/data_general_utils.py
+++ data_utils/data_general_utils.py
@@ -1,6 +1,6 @@
 import numpy as np
-from dtaidistance import dtw
-from dtaidistance import dtw_visualisation as dtwvis
+# from dtaidistance import dtw
+# from dtaidistance import dtw_visualisation as dtwvis
 
 
 def merge_two_dicts(x, y):
@@ -34,52 +34,52 @@ def noise_augmentation(x, y, mean=0, std=10, augmentation_factor=10, min_thresho
     return x, y
 
 
-def dtw_image_archive(target_ts_img, known_ts_img, duration=120):
-    # 120 * 8 * 16   120 * 8 * 16
-    # for target_ts_data
-    # (height, width, 3) # 3 is 3 channels for Red, Green, Blue
-    total_distance = 0
-    target_ts_img = target_ts_img.reshape((duration, -1))
-    known_ts_img = known_ts_img.reshape((duration, -1))
-
-    for channel in range(0, target_ts_img.shape[-1]):
-        distance, paths = dtw.warping_paths(target_ts_img[:, channel], known_ts_img[:, channel])
-        total_distance += distance
-
-    return total_distance
-
-
-def dtw_rd(target_ts_img, known_ts_img, duration=120, filename="warp.png"):
-    # 120 * 8 * 16   120 * 8 * 16
-    # for target_ts_data
-    # (height, width, 3) # 3 is 3 channels for Red, Green, Blue
-
-    # target_ts_img = np.abs(target_ts_img)
-    # known_ts_img = np.abs(known_ts_img)
-
-    target_ts_img[target_ts_img < 0] = 0
-    known_ts_img[known_ts_img < 0] = 0
-
-    tar_neg_speed_avg = target_ts_img[:, :, 0:8].mean(axis=(1, 2))
-    tar_pos_speed_avg = target_ts_img[:, :, 8:16].mean(axis=(1, 2))
-    known_neg_speed_avg = known_ts_img[:, :, 0:8].mean(axis=(1, 2))
-    known_pos_speed_avg = known_ts_img[:, :, 8:16].mean(axis=(1, 2))
-
-    distance_neg, paths_neg = dtw.warping_paths(s1=tar_neg_speed_avg, s2=known_neg_speed_avg)
-    distance_pos, paths_pos = dtw.warping_paths(s1=tar_pos_speed_avg, s2=known_pos_speed_avg)
-
-    distance = distance_neg + distance_pos
-    # for channel in range(0, target_ts_img.shape[-1]):
-    #     distance, paths = dtw.warping_paths(target_ts_img[:, channel], known_ts_img[:, channel])
-    #     total_distance += distance
-
-    path = dtw.warping_path(tar_neg_speed_avg, known_neg_speed_avg)
-    dtwvis.plot_warping(tar_neg_speed_avg.flatten(), known_neg_speed_avg.flatten(), path, filename=filename + '_neg')
-
-    path = dtw.warping_path(tar_pos_speed_avg, known_pos_speed_avg)
-    dtwvis.plot_warping(tar_pos_speed_avg.flatten(), known_pos_speed_avg.flatten(), path, filename=filename + '_pos')
-
-    return distance
+# def dtw_image_archive(target_ts_img, known_ts_img, duration=120):
+#     # 120 * 8 * 16   120 * 8 * 16
+#     # for target_ts_data
+#     # (height, width, 3) # 3 is 3 channels for Red, Green, Blue
+#     total_distance = 0
+#     target_ts_img = target_ts_img.reshape((duration, -1))
+#     known_ts_img = known_ts_img.reshape((duration, -1))
+#
+#     for channel in range(0, target_ts_img.shape[-1]):
+#         distance, paths = dtw.warping_paths(target_ts_img[:, channel], known_ts_img[:, channel])
+#         total_distance += distance
+#
+#     return total_distance
+#
+#
+# def dtw_rd(target_ts_img, known_ts_img, duration=120, filename="warp.png"):
+#     # 120 * 8 * 16   120 * 8 * 16
+#     # for target_ts_data
+#     # (height, width, 3) # 3 is 3 channels for Red, Green, Blue
+#
+#     # target_ts_img = np.abs(target_ts_img)
+#     # known_ts_img = np.abs(known_ts_img)
+#
+#     target_ts_img[target_ts_img < 0] = 0
+#     known_ts_img[known_ts_img < 0] = 0
+#
+#     tar_neg_speed_avg = target_ts_img[:, :, 0:8].mean(axis=(1, 2))
+#     tar_pos_speed_avg = target_ts_img[:, :, 8:16].mean(axis=(1, 2))
+#     known_neg_speed_avg = known_ts_img[:, :, 0:8].mean(axis=(1, 2))
+#     known_pos_speed_avg = known_ts_img[:, :, 8:16].mean(axis=(1, 2))
+#
+#     distance_neg, paths_neg = dtw.warping_paths(s1=tar_neg_speed_avg, s2=known_neg_speed_avg)
+#     distance_pos, paths_pos = dtw.warping_paths(s1=tar_pos_speed_avg, s2=known_pos_speed_avg)
+#
+#     distance = distance_neg + distance_pos
+#     # for channel in range(0, target_ts_img.shape[-1]):
+#     #     distance, paths = dtw.warping_paths(target_ts_img[:, channel], known_ts_img[:, channel])
+#     #     total_distance += distance
+#
+#     path = dtw.warping_path(tar_neg_speed_avg, known_neg_speed_avg)
+#     dtwvis.plot_warping(tar_neg_speed_avg.flatten(), known_neg_speed_avg.flatten(), path, filename=filename + '_neg')
+#
+#     path = dtw.warping_path(tar_pos_speed_avg, known_pos_speed_avg)
+#     dtwvis.plot_warping(tar_pos_speed_avg.flatten(), known_pos_speed_avg.flatten(), path, filename=filename + '_pos')
+#
+#     return distance
 
 
 def levenshtein_ratio_and_distance(s, t, ratio_calc=False):
