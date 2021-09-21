8e49a1d8f9593574e4381c57b3b1ffb002292539
HaowenWeiJohn <hwei@wpi.edu>
2021-09-20 22:04:33 -0400

    .sh trouble shoot

--- HPC_Final_Study2_Complete_Test_Class_Balance/participants_data_analysis/participant_run_all.sh
+++ HPC_Final_Study2_Complete_Test_Class_Balance/participants_data_analysis/participant_run_all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name="participant_run_all"
+#SBATCH --job-name="5User_CBA_Ultimate_Model"
 #SBATCH -D .
 #SBATCH --output=%x_%j.out
 #SBATCH --error=%x_%j.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --mem=32G
+#SBATCH --mem=60G
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
 
@@ -14,7 +14,7 @@
 
 
 module purge
-module load gcc/6.3.0 slurm/17.02.11 cudnn/8.1.1.33-11.2/3k5bbs63 cuda/11.0.2/ur4svoci 
+module load gcc/6.3.0 slurm/17.02.11 cudnn/8.1.1.33-11.2/3k5bbs63 cuda/11.0.2/ur4svoci
 
 
-python participant_run_all.sh
+python participant_run_all.py
