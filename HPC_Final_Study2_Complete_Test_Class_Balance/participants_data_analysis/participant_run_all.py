a3ccea79b95471d1c040449ca4dbf300600778df
HaowenWeiJohn <hwei@wpi.edu>
2021-10-02 16:54:05 -0400

    run 4, 5 last session and 1 for 7

--- HPC_Final_Study2_Complete_Test_Class_Balance/participants_data_analysis/participant_run_all.py
+++ HPC_Final_Study2_Complete_Test_Class_Balance/participants_data_analysis/participant_run_all.py
@@ -1,21 +1,22 @@
 import os
 import sys
 import platform
-sys.path.insert(1, '/home/hwei/work/HaowenWeiDeepLearning/IndexPenTrainingDir/IndexPen_Training/HPC_Final_Study2_Complete_Test_Class_Balance/participants_data_analysis')
 
+sys.path.insert(1,
+                '/home/hwei/work/HaowenWeiDeepLearning/IndexPenTrainingDir/IndexPen_Training/HPC_Final_Study2_Complete_Test_Class_Balance/participants_data_analysis')
 
-analysis_session = [(2,5)]
+analysis_session = [(4, 5), (5, 5), (7, 1)]
 
 os_type = platform.system()
 print(os_type)
-if os_type=="Windows":
+if os_type == "Windows":
     offset = ''
 else:
-    offset='python3 '
+    offset = 'python3 '
 
 for session_info in analysis_session:
     participant_name = 'participant_' + str(session_info[0])
-    session_name = 'session_'+ str(session_info[1])
+    session_name = 'session_' + str(session_info[1])
 
     # transfer current session, prepare lite model, model for next session
     participants_session_transfer_train = "participants_session_transfer_train.py"
@@ -30,20 +31,14 @@ for session_info in analysis_session:
     #
     # transfer train
 
-    os.system(offset+" ".join((participants_session_transfer_train, participant_name, session_name)))
-    os.system(offset+" ".join((participants_session_transfer_train_fresh_model, participant_name, session_name)))
+    os.system(offset + " ".join((participants_session_transfer_train, participant_name, session_name)))
+    os.system(offset + " ".join((participants_session_transfer_train_fresh_model, participant_name, session_name)))
     #
     # analysis transfer train best model and fresh model
-    os.system(offset+" ".join((participants_session_raw_acc_evaluation, participant_name, session_name)))
+    os.system(offset + " ".join((participants_session_raw_acc_evaluation, participant_name, session_name)))
 
-    os.system(offset+" ".join((participants_session_raw_prediction, participant_name, session_name)))
+    os.system(offset + " ".join((participants_session_raw_prediction, participant_name, session_name)))
 
-    os.system(offset+" ".join((participants_session_raw_prediction_evaluation, participant_name, session_name)))
-
-
-
-
+    os.system(offset + " ".join((participants_session_raw_prediction_evaluation, participant_name, session_name)))
 
 # plot current final result. Current raw accuracy and current evaluation
-
-
