952faa05b14d4205365cd3e951750f8a8fa674b6
HaowenWeiJohn <hwei@wpi.edu>
2021-10-28 23:11:53 -0400

    add run_all_3

---
+++ HPC_Final_Study1_Complete_Test/HPC_Final_Study1_Complete_Test_Shallow/Sub3_LeaveOut_test/training_learn.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name="Sub3_S_LeaveOut_test"
+#SBATCH -D .
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=80G
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+
+#SBATCH --partition=long
+#SBATCH --time=150:00:00
+
+
+module purge
+module load gcc/6.3.0 slurm/17.02.11 cudnn/8.1.1.33-11.2/3k5bbs63 cuda/11.0.2/ur4svoci 
+
+
+python training_script.py
---
+++ HPC_Final_Study2_Complete_Test_Class_Balance_run_all/participants_data_analysis/participant_run_all_3.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import platform
+
+sys.path.insert(1,
+                '/home/hwei/work/HaowenWeiDeepLearning/IndexPenTrainingDir/IndexPen_Training/HPC_Final_Study2_Complete_Test_Class_Balance/participants_data_analysis')
+
+
+analysis_session = [
+    # (9,5),
+    # (18,3),
+    # (12, 4),
+    # (20,4),
+    # (21,4)
+    # (21, 1)
+    (19, 1), (19, 2), (19, 3), (19, 4), (19, 5),
+    (20, 1), (20, 2), (20, 3), (20, 4), (20, 5)
+
+]
+
+os_type = platform.system()
+print(os_type)
+if os_type == "Windows":
+    offset = ''
+else:
+    offset = 'python3 '
+
+for session_info in analysis_session:
+    participant_name = 'participant_' + str(session_info[0])
+    session_name = 'session_' + str(session_info[1])
+
+    # transfer current session, prepare lite model, model for next session
+    participants_session_transfer_train = "participants_session_transfer_train.py"
+
+    participants_session_transfer_train_fresh_model = "participants_session_transfer_train_fresh_model.py"
+    # analysis current session using previous model, raw acc
+    participants_session_raw_acc_evaluation = "participants_session_raw_acc_evaluation.py"
+
+    # analysis current session using debouncer algorithm
+    participants_session_raw_prediction = "participants_session_raw_prediction.py"
+    participants_session_raw_prediction_evaluation = "participants_session_raw_prediction_evaluation.py"
+    #
+    # transfer train
+
+    os.system(offset + " ".join((participants_session_transfer_train, participant_name, session_name)))
+    os.system(offset + " ".join((participants_session_transfer_train_fresh_model, participant_name, session_name)))
+    #
+    # analysis transfer train best model and fresh model
+    os.system(offset + " ".join((participants_session_raw_acc_evaluation, participant_name, session_name)))
+
+    os.system(offset + " ".join((participants_session_raw_prediction, participant_name, session_name)))
+
+    os.system(offset + " ".join((participants_session_raw_prediction_evaluation, participant_name, session_name)))
+
+# plot current final result. Current raw accuracy and current evaluation
