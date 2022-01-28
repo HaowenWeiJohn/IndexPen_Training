1e066bc0385177af59098226c75723ca29fac21b
HaowenWeiJohn <hwei@wpi.edu>
2022-01-28 16:43:37 -0500

    double filter

--- HPC_Final_Study1_Complete_Test/5User_Ultimate_Model_RA_RD_Combine_double_filter/learn.sh
+++ HPC_Final_Study1_Complete_Test/5User_Ultimate_Model_RA_RD_Combine_double_filter/learn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name="5User_Ultimate_Model_RA_RD_Combine"
+#SBATCH --job-name="5User_Ultimate_Model_RA_RD_Combine_Double_Filter"
 #SBATCH -D .
 #SBATCH --output=%x_%j.out
 #SBATCH --error=%x_%j.err
